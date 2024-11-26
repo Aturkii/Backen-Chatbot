a201cfbfa354059a791b2442437de2c51122deed
Aturkii <Ahturki222@gmail.com>
2024-11-26 23:50:41 +0200

    dsdssd

--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import * as prodcutController from "./product.controller.js";
-import * as productValidation from "./product.validation.js";
-import { validation } from './../../middlewares/validation.js';
 
 const router = Router()
 
@@ -15,18 +13,14 @@ router.get('/product',
 
 //^ Add Product
 router.post('/',
-  validation(productValidation.productSchema),
   prodcutController.createProduct)
 
 //^ Update Product
 router.put('/',
-  validation(productValidation.productName, 'query'),
-  validation(productValidation.updateProductSchema),
   prodcutController.updateProduct)
 
 //^ Delete Product
 router.delete('/',
-  validation(productValidation.productName, 'query'),
   prodcutController.deleteProduct)
 
 export default router;
